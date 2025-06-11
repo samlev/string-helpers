6233c3c6634a7d36916d5128a4cfdc75765adf39
samlev <462466+samlev@users.noreply.github.com>
2025-06-11 05:05:24 +0000

    [BOT] Build distribution assets for 1.2.0

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,3 +1,14 @@
+import camel from "./camel";
+import headline from "./headline";
+import lcfirst from "./lcfirst";
+import normalize from "./normalize";
+import random from "./random";
+import regex from "./regex";
+import snake from "./snake";
+import squish from "./squish";
+import studly from "./studly";
+import ucfirst from "./ucfirst";
+export { camel, headline, lcfirst, normalize, random, regex, snake, squish, studly, ucfirst };
 declare const _default: {
     camel: (str: string) => string;
     headline: (str: string) => string;
--- dist/regex/index.d.ts
+++ dist/regex/index.d.ts
@@ -1,3 +1,6 @@
+import path from "./path";
+import sanitize from "./sanitize";
+export { sanitize, path };
 declare const _default: {
     sanitize: (str: string) => string;
     path: (str: string, start?: string, end?: string) => RegExp;
